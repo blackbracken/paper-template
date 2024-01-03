0989baacb97610873caeae6b0ccd8321082fe02f
blackbracken <me@bracken.black>
2024-01-03 13:36:15 +0900

    initialize project

---
+++ src/test/java/papertemplate/SampleTest.java
@@ -0,0 +1,13 @@
+package papertemplate;
+
+import black.bracken.papertemplate.Main;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.junit.jupiter.api.BeforeEach;
+
+public final class SampleTest {
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+}
